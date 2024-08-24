26080ad10ecea985693ea684b515ca23a6d5b343
beneditotavio <otavio.bene@outlook.com>
2024-08-23 22:33:14 -0300

    feature: create global services

---
+++ ui/src/services/list.services.ts
@@ -0,0 +1,37 @@
+import { WordsTree } from "../interfaces/wordsTree.interface";
+
+export class ListService {
+  public reduceNodesInList = (list: any[]): number[] => {
+    return list.reduce((acc: any, item: any) => {
+      if (acc.includes(item.node)) return acc;
+      else {
+        acc.push(item.node);
+        return acc;
+      }
+    }, []);
+  };
+
+  public filterWordsbyParent = (list: any[], parent: string): WordsTree[] => {
+    return list.filter((item) => item.parent === parent);
+  };
+
+  public filterWordsbyNode = (list: any[], node: number): WordsTree[] => {
+    return list.filter((item) => item.node === node);
+  };
+
+  public filterWordsbyName = (list: any[], name: string): WordsTree[] => {
+    return list.filter((item) => item.name === name);
+  };
+
+  public reduceWordsByName = (
+    list: any[]
+  ): { name: string; node: number }[] => {
+    return list.reduce((acc: any, item: any) => {
+      if (acc.includes(item.name)) return acc;
+      else {
+        acc.push({ name: item.name, node: item.node });
+        return acc;
+      }
+    }, []);
+  };
+}
