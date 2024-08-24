a40d7598cbd86c7f2113983b7daad4f052b56dd1
beneditotavio <otavio.bene@outlook.com>
2024-08-23 22:38:47 -0300

    chore: config default  files

---
+++ ui/src/app.tsx
@@ -0,0 +1,10 @@
+import styles from "./app.module.css";
+import { RoutesDOM } from "./routes";
+
+export function App() {
+  return (
+    <div className={styles.body}>
+      <RoutesDOM />
+    </div>
+  );
+}
